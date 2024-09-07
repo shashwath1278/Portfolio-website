7303458630d835809521ae633f73254bd2a6d2b5
shashwath1278 <drs.prabhu2018@gmail.com>
2024-09-07 23:19:40 +0530

    second comit

--- src/components/New/New.js
+++ src/components/New/New.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './New.css'
-import shash1 from '../../assets/shash1.png'
+import shashwath from '../../assets/shashwath-img.jpg'
 import Ril from '../Ril/Ril'
 
 const New = () => {
   return (
     <section id="home" className="h">
     <div className="newcomp">
-     <img src={shash1} height="300" width="300" alt=""></img>
+     <img src={shashwath} height="320" width="220" alt=""></img>
      <h1><Ril/></h1>
      <p><h4>A second year undergrad at MSRIT, currently learning front-end dev and ethical hacking.</h4> </p>
      <div className="action"></div>
